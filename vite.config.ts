bc90df7c60a1400f4793c4d7fe04de348870c587
Rezki Ade Pratama  Putra <rizkie.kudo@gmail.com>
2025-08-13 14:22:38 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/me/' : '/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  base: '/me/', // <- pastikan ini sesuai nama repo
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
